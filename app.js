3b176fe73ccd126933a3ff68931505bafd58df10
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-06-03 14:31:49 -0400

    m

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-var year, yearJohn, yearMark;
-now = 2018;
-ageJohn = 28;
-ageMark = 33;
+// var year, yearJohn, yearMark;
+// now = 2018;
+// ageJohn = 28;
+// ageMark = 33;
 
-//Math operators 
+// //Math operators 
 
-yearJohn = now - ageJohn;
-yearMark = now- ageMark;
+// yearJohn = now - ageJohn;
+// yearMark = now- ageMark;
 
-console.log(yearJohn);
+// console.log(yearJohn);
 
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 10);
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
 
-//logical operators 
+// //logical operators 
 
-var johnOlder = ageJohn < ageMark;
-console.log(johnOlder);
+// var johnOlder = ageJohn < ageMark;
+// console.log(johnOlder);
+
+// //typeof operator 
+
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof 'Mark is older than john ')
+// var x;
+// console.log(typeof x);
+
+//operator precedence 
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+//Multiple operators
+var isFullAge = now - yearJohn >= fullAge; 
+console.log(isFullAge);
+
+//Grouping
+var  ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average)
+
+//Multiple assingment 
+
+var x, y;
+x = (3+5)*4-6;
+console.log(x);
