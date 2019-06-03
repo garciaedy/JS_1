56014de02cbf09bc7c86ea2f61fa5f58edf6a31f
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-06-03 14:00:21 -0400

    m

---
+++ app.js
@@ -0,0 +1,20 @@
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+//Math operators 
+
+yearJohn = now - ageJohn;
+yearMark = now- ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+//logical operators 
+
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
